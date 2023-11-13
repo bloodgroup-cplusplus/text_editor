7ded07ea13dc0638979c244ba8eae741549c2f9c
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-11-13 14:46:24 +0530

    added toggler

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -10,3 +10,31 @@ import {
 } from "lucide-react"
 
 import {Toggle} from "./ui/toggle"
+
+type Props = {
+    editor:Editor | null
+}
+export function Toolbar({editor}:Props){
+    if(!editor){
+        return null
+    }
+    return (
+        <div className="border border-input bg-transparent rounded-">
+            <Toggle
+                size="sm"
+                pressed={editor.isActive("heading")}
+                onPressedChange={()=>
+                    editor.chain().focus().toggleHeading({level:2}).run()
+                }
+            >
+                <Heading2 className="h-4 w-4"/>
+
+            </Toggle>
+
+            <Toggle
+                size="sm"
+                pressed={editor.isActive("bold")}
+                onPressedChange={()=>editor.chain().focus().toggleBold()}
+        </div>
+    )
+}
