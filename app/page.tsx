586c736aa379f4d4a351913f6197b6f83e35f5c8
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-11-13 13:10:39 +0530

    added the zod database rules

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,16 @@ import {
 } from "@/components/ui/form"
 import {useForm} from "react-hook-form"
 import * as z from "zod"
+
+const formSchema = z.object ({
+  title:z.string().min(5,{message:'Hey the title is not long enough'})
+  .max(100,{message:"Its too long"}),
+  price:z.number().min(5,{message:"Hey the title is not long enough"}),
+  description:z.string()
+  .min(5,{message:"Hey the title is not long enough"})
+  .max(100,{message:"Its too long"})
+  .trim(),
+})
 export default function Home() {
     const form = useForm({
       mode:'onChange',
